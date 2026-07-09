e9c57c336b12988b4e2baceb166af018d2e3f799
Heikki Laivamaa <heikkilaivamaa@users.noreply.github.com>
2026-07-09 12:32:11 +0300

    Fix Express 5 wildcard routing error

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 8080;
 app.use(express.static(path.join(__dirname, 'dist')));
 
 // Kaikki muut pyynnöt ohjataan index.html (React Routeria varten)
-app.get('*', (req, res) => {
+app.use((req, res) => {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
