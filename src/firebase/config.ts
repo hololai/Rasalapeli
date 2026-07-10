b01884f14af955e8e3922033513159e69cdaa93f
Heikki Laivamaa <heikkilaivamaa@users.noreply.github.com>
2026-07-10 10:59:21 +0300

    fix: revert authDomain to restore login functionality

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -5,7 +5,7 @@ import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
   apiKey: "AIzaSyArSViLuagzKbiJTO5uBv85eAIQRRAgOtM",
-  authDomain: "sukukuvat.fi",
+  authDomain: "kuvaohjelma.firebaseapp.com",
   projectId: "kuvaohjelma",
   storageBucket: "kuvaohjelma.firebasestorage.app",
   messagingSenderId: "150396211718",
